32ad5c30f8ef8ad03613eafcb6ecacc141220b5c
julien_ripet <ripetjulien@gmail.com>
2019-06-17 12:14:44 +0200

    add gitignote + start the basic model

---
+++ api/models/todoListModel.js
@@ -0,0 +1,23 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TaskSchema = new Schema({
+    name:{
+        type:String,
+        required : 'Kindly enter the name of the task'
+    },
+    Create_date:{
+        type:Date,
+        default:Date.now
+    },
+    status:{
+        type:[{
+            type:String,
+            enum:['Pending','Ongoing','Completed']
+        }],
+        default:['pending']
+    }
+});
+
+module.exports = mongoose.model('Tasks',TaskSchema);
