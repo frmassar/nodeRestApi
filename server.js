43d2804f04cd588e3f93d3e8387f04cddf7ef05b
julien_ripet <ripetjulien@gmail.com>
2019-06-18 11:28:02 +0200

    fix mergeconflit

--- server.js
+++ server.js
@@ -1,25 +1,5 @@
 var express = require('express'),
-<<<<<<< HEAD
-  app = express(),
-  port = process.env.PORT || 3000;
-  mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'),
-  bodyParser = require('body-parser');
 
-
-  mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://localhost/Tododb');
-
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.json());
-
-  var routes = require ('./api/routes/todoListRoute');
-  routes(app);
-
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
-=======
 
 app = express(),
 port = process.env.PORT || 3000,
@@ -43,4 +23,3 @@ app.use(function(req, res){
 })
 
 console.log('todo list RESTful API server started on: ' + port);
->>>>>>> 32ad5c30f8ef8ad03613eafcb6ecacc141220b5c
